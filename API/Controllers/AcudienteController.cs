98caece2ce858503271fc0fe74aca0fa15a8501e
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-07-13 09:26:30 -0500

    feat: :construction: EndPoint Acudiente
    endpoint de acudiente creado

--- API/Controllers/AcudienteController.cs
+++ API/Controllers/AcudienteController.cs
@@ -18,7 +18,15 @@ public class AcudienteController : BaseApiController
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<IEnumerable<Acudiente>>> Get()
     {
-         var nameVar = await _context.Acudientes.ToListAsync();
-         return Ok(nameVar);
+        var nameVar = await _context.Acudientes.ToListAsync();
+        return Ok(nameVar);
+    }
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Get(int id)
+    {
+        var acudiente = await _context.Acudientes.FindAsync(id);
+        return Ok(acudiente);
     }
 }
